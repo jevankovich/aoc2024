15cfc3f3e4491f797c228a9e06d9ebcc934f25ac
Justin Evankovich <justin@evanko.dev>
2024-11-30 23:57:59 -0500

    Add runner for all days, and gitignore pycache

--- day1.py
+++ day1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-def main():
+def main(lines: list[str]):
+	print(lines[0].rstrip())
+
+if __name__ == '__main__':
 	with open(sys.argv[1], 'r') as f:
 		lines = f.readlines()
 
-	print(lines[0])
-
-if __name__ == '__main__':
-	main()
+	main(lines)
---
+++ runall.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import importlib
+import time
+
+def main():
+	for day in range(1, 26):
+		name = "day" + str(day)
+		try:
+			module = importlib.import_module(name)
+			with open(f"{name}.txt", 'r') as f:
+				lines = f.readlines()
+
+			print(name)
+			start_time = time.perf_counter()
+			module.main(lines)
+			end_time = time.perf_counter()
+			print(f"{end_time - start_time:.3g} seconds")
+			print()
+
+		except ModuleNotFoundError:
+			pass
+
+if __name__ == '__main__':
+	main()
