d71f9e7396f0ce3dff068a6e0960b2b9d79f8a41
Justin Evankovich <justin@evanko.dev>
2024-12-01 00:05:36 -0500

    Completed day1

--- day1.py
+++ day1.py
@@ -2,7 +2,27 @@
 import sys
 
 def main(lines: list[str]):
-	print(lines[0].rstrip())
+	left = []
+	right = []
+	for line in lines:
+		l, r = line.split()
+		left.append(int(l))
+		right.append(int(r))
+
+	left.sort()
+	right.sort()
+
+	total_dist = 0
+	for l, r in zip(left, right):
+		total_dist += l - r if l > r else r - l
+
+	print(total_dist)
+
+	similarity = 0
+	for l in left:
+		similarity += l * right.count(l)
+
+	print(similarity)
 
 if __name__ == '__main__':
 	with open(sys.argv[1], 'r') as f:
