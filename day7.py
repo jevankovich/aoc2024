6be89d8e0dbfa0a53da4568938394971b2c41a73
Justin Evankovich <justin@evanko.dev>
2024-12-07 00:33:56 -0500

    Optimized day 7 by a severe margin

--- day7.py
+++ day7.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
 import sys
 
+def possible(result, terms, cat=False):
+	results = {result}
+	for term in reversed(terms[1:]):
+		next_results = set()
+		for result in results:
+			if result > term:
+				next_results.add(result - term)
+
+			if result % term == 0:
+				next_results.add(result // term)
+
+			if cat:
+				sresult = str(result)
+				sterm = str(term)
+				if len(sresult) > len(sterm) and sresult.endswith(sterm):
+					next_results.add(int(sresult.removesuffix(sterm)))
+
+		results = next_results
+
+	return terms[0] in results
+
+
 def main(lines: list[str]):
 	calibration = 0
 	calibration_cat = 0
@@ -9,23 +31,10 @@ def main(lines: list[str]):
 		result = int(result)
 		terms = list(map(int, terms.split(' ')))
 
-		possible = {terms[0]}
-		for term in terms[1:]:
-			mul = {x * term for x in possible}
-			add = {x + term for x in possible}
-			possible = mul | add
-
-		if result in possible:
+		if possible(result, terms):
 			calibration += result
 
-		possible_cat = {terms[0]}
-		for term in terms[1:]:
-			mul = {x * term for x in possible_cat}
-			add = {x + term for x in possible_cat}
-			cat = {int(str(x) + str(term)) for x in possible_cat}
-			possible_cat = mul | add | cat
-
-		if result in possible_cat:
+		if possible(result, terms, True):
 			calibration_cat += result
 
 	print(calibration)
